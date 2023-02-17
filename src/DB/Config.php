993a6d7e25a9f4e3644f823459ae836407584564
Ethan Matthew Posner <posnerem@stumail.csci.viu.ca>
2023-02-16 19:24:24 -0800

    Setup database connection

---
+++ src/DB/Config.php
@@ -0,0 +1,22 @@
+<?php
+
+require ('LoadEnv/LoadEnv.php');
+
+class Config {
+    /**
+     * Stores configuration details for this project, such as
+     * the URL for the MySQL database
+     */
+    public $DB_HOST, $DB_PORT, $DB_NAME, $CHARSET, $DB_USER, $DB_PASS, $DB_URL;
+
+    public function __construct() {
+        load_dotenv();
+        $this->DB_HOST = $_ENV['DB_HOST'];
+        $this->DB_PORT = $_ENV['DB_PORT'];
+        $this->DB_NAME = $_ENV['DB_NAME'];
+        $this->CHARSET = 'utf8';
+        $this->DB_USER = $_ENV['DB_USER'];
+        $this->DB_PASS = $_ENV['DB_PASS'];
+        $this->DB_URL = "mysql:host={$this->DB_HOST};port={$this->DB_PORT};dbname={$this->DB_NAME};charset={$this->CHARSET}";
+    }
+}
